898d09c516e333d3fdba4cbffaf161e4ee11c4ea
Supramanium S B <saidhasun0407@gmail.com>
2024-11-03 18:37:55 +0530

    bug

--- server/src/routes/register.route.js
+++ server/src/routes/register.route.js
@@ -7,7 +7,7 @@ const {forgotPassword,resetPassword} = require("../controllers/ForgotPassword.co
 router.get("/getData",registerCtrl.getData)
 
 router.post("/register",registerCtrl.regPost)
-router.post("/login/:token",verifyEmail,registerCtrl.loginPost)
+router.post("/login",registerCtrl.loginPost)
 // router.post("/otplogin",registerCtrl.otpLogin)
 router.post("/forgotpassword", forgotPassword);
 router.post("/resetpass/:token", resetPassword);
