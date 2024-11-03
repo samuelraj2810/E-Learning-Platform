e94a6687c6910bd900e4359d6bad653dc48150d8
Ajith kanna T <108063094+ajithkanna1234@users.noreply.github.com>
2024-11-03 21:27:12 +0530

    .

--- client/src/Components/ProfileDetails.jsx
+++ client/src/Components/ProfileDetails.jsx
@@ -1,14 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import CustomInput from "./Common/CustomInput";
+import { GET } from "./ApiFunction/ApiFunction";
 
 const ProfileDetails = () => {
-    const [token,setToken] = useState(null)
-    console.log(token)
-    useEffect(()=>{
-        setToken(localStorage.getItem("token"))
-    },[])
-  return (
-    <div>ProfileDetails</div>
-  )
-}
+  const [token, setToken] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+    console.log(data);
+    
+  const fetchData = async () => {
+    try {
+      const result = await GET("http://localhost:3000/getData");
+      setData(result?.data || []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    setToken(localStorage.getItem("token"));
+  }, []);
 
-export default ProfileDetails
+  return (
+    <div className="h-screen max-w-screen p-4">
+      <h1 className="lg:text-2xl font-bold my-4">Profile details</h1>
+      <form className="mt-10 border lg:w-4/5 lg:mx-auto flex flex-col md:gap-20 gap-10 md:flex-row rounded-lg p-2 md:p-4 lg:p-6">
+        <div className="md:flex-1 grid gap-4">
+            {data?.map((each)=><CustomInput className="" title={each.key} />)}
+        </div>
+        <div className="md:flex-1 grid gap-4">
+          <CustomInput className="" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileDetails;
