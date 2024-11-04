8e8c3a43191d0d25c8d1c02b36854509864a4954
Supramanium S B <saidhasun0407@gmail.com>
2024-11-04 13:40:49 +0530

    created new database

---
+++ server/src/models/UserDetails..model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { v4 } = require("uuid");
+
+const userSch = new mongoose.Schema(
+
+{
+    name:String,
+    phonenumber: Number,
+    age:Number,
+    sex:String,
+    email: String,
+    designation: String,
+    _id: {
+      type: String,
+      default: v4,
+    },
+    userId: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+const userDetails = mongoose.model("UserDetails",userSch)
+
+module.exports= userDetails
