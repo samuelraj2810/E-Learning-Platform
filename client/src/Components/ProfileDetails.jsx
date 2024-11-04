80723cfa588a3fdbc17cad58bb5b525d14b3c4d3
Ajith kanna T <ajithkanna78@gmail.com>
2024-11-04 03:58:49 -0800

    profile details

--- client/src/Components/ProfileDetails.jsx
+++ client/src/Components/ProfileDetails.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
 import CustomInput from "./Common/CustomInput";
 import { GET } from "./ApiFunction/ApiFunction";
+import CustomButton from "./Common/CustomButton";
 
 const ProfileDetails = () => {
   const [token, setToken] = useState(null);
   const [data, setData] = useState([]);
+  const titles = [
+    "username",
+    "email",
+    "gender",
+    "address",
+    "number",
+    "designation",
+  ];
   const [isLoading, setIsLoading] = useState(true);
+  const [isupdate, setIsupdate] = useState(true);
   console.log(data);
   const fetchData = async () => {
     try {
@@ -25,30 +35,45 @@ const ProfileDetails = () => {
     fetchData();
     setToken(localStorage.getItem("token"));
   }, []);
-
+  const handleButtonClick = () => {
+    setIsupdate((p) => !p);
+  };
   return (
-    <div className="h-screen max-w-screen p-4">
-      <h1 className="lg:text-2xl font-bold my-4">Profile details</h1>
-      <form className="mt-10 border lg:w-4/5 lg:mx-auto flex flex-col md:gap-20 gap-10 md:flex-row rounded-lg p-2 md:p-4 lg:p-6">
-        <div className="md:flex-1 grid gap-4">
-          {data?.slice(0,1).map((user) =>
-            Object.keys(user).map((key) => (
-              <CustomInput
-                key={user._id + key} // Ensure each key is unique
-                className=""
-                title={key} // Use the key as the title
-                value={user[key]} // Use the value for the corresponding key
-                // onChange={(value) => handleChange(user._id, key, value)} // Update function
-              />
-            ))
-          )}
+    <div className="h-fit bg-gray-100 max-w-screen p-4">
+      <form className="my-10 shadow bg-white lg:mx-auto flex flex-col md:flex-row md:gap-20 gap-10 rounded-lg p-4 lg:p-6">
+        <div className="flex gap-4 justify-between md:flex-col h-fit">
+          <h1 className="lg:text-2xl font-bold">Profile details</h1>
+          <CustomButton
+            title={isupdate ? "update" : "Submit"}
+            onClick={handleButtonClick}
+            className={` text-xs ${
+              isupdate && "bg-green-500"
+            } capitalize tracking-wider`}
+            color="solid"
+          />
         </div>
+        <div className="md:flex-1 flex flex-col gap-4">
+          {titles.map((title) => (
+            <CustomInput
+              disabled={isupdate}
+              className="text-xs lg:text-base"
+              containerClassName="mx-2"
+              titleClassName="text-xs lg:text-base"
+              title={title}
+              value={data.length > 0 ? data[0][title] : ''}
+            />
+          ))}
+        </div>
+
         <div className="md:flex-1 grid gap-4">
-        {data?.slice(1,2).map((user) =>
+          {data?.slice(1, 2).map((user) =>
             Object.keys(user).map((key) => (
               <CustomInput
+                disabled={isupdate}
                 key={user._id + key} // Ensure each key is unique
-                className=""
+                className="text-xs lg:text-base"
+                containerClassName="mx-2"
+                titleClassName="text-xs lg:text-base"
                 title={key} // Use the key as the title
                 value={user[key]} // Use the value for the corresponding key
                 // onChange={(value) => handleChange(user._id, key, value)} // Update function
