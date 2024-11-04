16ea87780a81598981ae855affc7182ae5c1ef63
Ajith kanna T <108063094+ajithkanna1234@users.noreply.github.com>
2024-11-04 21:05:00 +0530

    ..

--- client/src/Components/ProfileDetails.jsx
+++ client/src/Components/ProfileDetails.jsx
@@ -2,8 +2,25 @@ import React, { useEffect, useState } from "react";
 import CustomInput from "./Common/CustomInput";
 import { GET } from "./ApiFunction/ApiFunction";
 import CustomButton from "./Common/CustomButton";
-
+import { Radio } from 'antd';
+import { useCustomMessage } from "./Common/CustomMessage";
+const options = [
+  {
+    label: 'Apple',
+    value: 'Apple',
+  },
+  {
+    label: 'Pear',
+    value: 'Pear',
+  },
+  {
+    label: 'Orange',
+    value: 'Orange',
+    title: 'Orange',
+  },
+];
 const ProfileDetails = () => {
+  const showMessage = useCustomMessage();
   const [token, setToken] = useState(null);
   const [data, setData] = useState([]);
   const titles = [
@@ -16,7 +33,12 @@ const ProfileDetails = () => {
   ];
   const [isLoading, setIsLoading] = useState(true);
   const [isupdate, setIsupdate] = useState(true);
+  const [value1, setValue1] = useState('Apple');
   console.log(data);
+  const onChange1 = ({ target: { value } }) => {
+    console.log('radio1 checked', value);
+    setValue1(value);
+  };
   const fetchData = async () => {
     try {
       const result = await GET("http://localhost:3000/getData");
@@ -37,12 +59,13 @@ const ProfileDetails = () => {
   }, []);
   const handleButtonClick = () => {
     setIsupdate((p) => !p);
+    showMessage("success","clicked")
   };
   return (
-    <div className="h-fit bg-gray-100 max-w-screen p-4">
-      <form className="my-10 shadow bg-white lg:mx-auto flex flex-col md:flex-row md:gap-20 gap-10 rounded-lg p-4 lg:p-6">
-        <div className="flex gap-4 justify-between md:flex-col h-fit">
-          <h1 className="lg:text-2xl font-bold">Profile details</h1>
+    // <div className="h-[100vh] bg-gray-100 max-w-screen p-4">
+      <form className="overflow-hidden bg-white lg:mx-auto flex flex-col rounded-lg p-4 lg:p-6">
+        <div className="flex gap-4 justify-between h-fit">
+          <h1 className="lg:text-2xl text-xl font-bold">Profile details</h1>
           <CustomButton
             title={isupdate ? "update" : "Submit"}
             onClick={handleButtonClick}
@@ -52,7 +75,8 @@ const ProfileDetails = () => {
             color="solid"
           />
         </div>
-        <div className="md:flex-1 flex flex-col gap-4">
+        <div className="md:flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+          {/* <Radio.Group options={options} onChange={onChange1} value={value1} className="border h-fit"/> */}
           {titles.map((title) => (
             <CustomInput
               disabled={isupdate}
@@ -60,11 +84,23 @@ const ProfileDetails = () => {
               containerClassName="mx-2"
               titleClassName="text-xs lg:text-base"
               title={title}
-              value={data.length > 0 ? data[0][title] : ''}
+              value={data.length > 0 ? [...data.values()][0][title] : ''}
+              // onChange={(e)}
+            />
+          ))}
+          {titles.map((title) => (
+            <CustomInput
+              disabled={isupdate}
+              className="text-xs lg:text-base"
+              containerClassName="mx-2"
+              titleClassName="text-xs lg:text-base"
+              title={title}
+              value={data.length > 0 ? [...data.values()][0][title] : ''}
+              // onChange={(e)}
             />
           ))}
         </div>
-        <div className="md:flex-1 flex flex-col gap-4">
+        {/* <div className="md:flex-1 flex flex-col gap-4">
           {titles.map((title) => (
             <CustomInput
               disabled={isupdate}
@@ -75,9 +111,9 @@ const ProfileDetails = () => {
               value={data.length > 0 ? data[1][title] : ''}
             />
           ))}
-        </div>
+        </div> */}
       </form>
-    </div>
+    // </div>
   );
 };
 
