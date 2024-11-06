d11aef95f02df1e104b3b70ebd590f3ca174e1b1
Supramanium S B <saidhasun0407@gmail.com>
2024-11-06 20:55:24 +0530

    .

--- server/src/controllers/UserDetails.controller.js
+++ server/src/controllers/UserDetails.controller.js
@@ -20,9 +20,8 @@ const getData = async(req,res) =>{
 const editData = async(req,res) =>{
     try{
         const userId = req.userId
-        const data = await userDetails.updateOne({userId},req.body)
-        const updatedData = await userDetails.findOne({userId})
-        res.json({updatedData,message:"Data updated successfully"})
+        const data = await userDetails.findOneAndUpdate({userId},req.body,{new:true})
+        res.json({data,message:"Data updated successfully"})
         // console.log(data);
         
     }
