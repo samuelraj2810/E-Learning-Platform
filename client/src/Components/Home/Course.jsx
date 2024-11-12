9533d00483e1bbb3bdb11f460ccd87d7aee99196
Supramanium S B <saidhasun0407@gmail.com>
2024-11-12 15:24:35 +0530

    .

--- client/src/Components/Home/Course.jsx
+++ client/src/Components/Home/Course.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import javaScriptBaicsImage from "../../Assets/CourseCatalog/image1.png";
-// import Reactfundamental from "../../Assets/CourseCatalog/pngwing.png";
-// import nodejs from "../../Assets/CourseCatalog/image3.png";
-// import css from "../../Assets/CourseCatalog/image4.png";
-// import API from "../../Assets/CourseCatalog/image5.png";
+import javaScriptBaicsImage from "../../Assets/CourseCatalog/Js.jpg";
+import Reactfundamental from "../../Assets/CourseCatalog/React.jpg";
+import nodejs from "../../Assets/CourseCatalog/Node.jpg";
+import css from "../../Assets/CourseCatalog/css.jpg";
 
 const courses = [
-  { id: 1,  title: "JavaScript Basics", instructor: "John Doe", price: "$49", category: "Programming", rating: 4.5 },
-  { id: 2,  title: "React Fundamentals", instructor: "Jane Smith", price: "$59", category: "Frontend Development", rating: 4.7 },
-  { id: 3,  title: "Node.js for Beginners", instructor: "Alex Brown", price: "$39", category: "Backend Development", rating: 4.3 },
-  { id: 4, title: "CSS for Beginners", instructor: "Emily Davis", price: "$29", category: "Frontend Development", rating: 4.8 },
-  { id: 5, title: "API Learning", instructor: "Michael Clark", price: "$45", category: "Backend Development", rating: 4.6 },
+  { id: 1, image:"" , title: "JavaScript Basics", instructor: "John Doe", price: "$49", category: "Programming", rating: 4.5 },
+  { id: 2, image:"" ,title: "React Fundamentals", instructor: "Jane Smith", price: "$59", category: "Frontend Development", rating: 4.7 },
+  { id: 3,image:"" , title: "Node.js for Beginners", instructor: "Alex Brown", price: "$39", category: "Backend Development", rating: 4.3 },
+  { id: 4,image:"" ,title: "CSS for Beginners", instructor: "Emily Davis", price: "$29", category: "Frontend Development", rating: 4.8 }
 ]
 
 const Course = () => {
@@ -24,12 +22,12 @@ const Course = () => {
         <h1 className="text-3xl font-bold mb-8">Course Catalog</h1>
 
         {/* Course Cards */}
-        <div className=" p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className=" p-14 flex flex-wrap justify-evenly ">
           {courses.length > 0 ? (
             courses.map(course => (
               <div
                 key={course.id}
-                className="bg-white shadow-md rounded-lg p-4 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-blue-50"
+                className="bg-white flex-col w-[350px] shadow-md rounded-lg p-4 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-blue-50"
               >
                 {/* Image section */}
                 <img
