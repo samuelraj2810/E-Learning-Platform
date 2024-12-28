6bf36a41c7407381c8996688c51ab24f40c9bcab
Ajith kanna T <108063094+ajithkanna1234@users.noreply.github.com>
2024-12-28 22:32:10 +0530

    bug

--- client/src/Components/Student/Course/CourseCards.jsx
+++ client/src/Components/Student/Course/CourseCards.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import {GET} from "../../ApiFunction/ApiFunction"
+import { GET } from "../../ApiFunction/ApiFunction";
 import axios from "axios";
 
 const CourseCards = () => {
-
   const [coursedata, setCoursedata] = useState([]);
 
   useEffect(() => {
@@ -12,26 +11,25 @@ const CourseCards = () => {
 
   const getData = async () => {
     const token = sessionStorage.getItem("token");
-    const result = await GET("http://localhost:3000/getinstcourse", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (result.data) {
-      setCoursedata(result.data);
+    const result = await GET("http://localhost:3000/getallcourse", token);
+    if (result) {
+      setCoursedata(result);
     } else {
       setCoursedata([]);
     }
   };
-  console.log(coursedata)
+  console.log(coursedata);
 
   return (
     <div className="h-fit w-full p-2 lg:pt-4 grid gap-2 lg:gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-      <div className="border rounded-lg p-4 max-h-48 min-h-48 lg:max-h-72 lg:min-h-80 hover:shadow-md transition-all duration-500 bg-white">
-        <div className="border h-1/2 w-full">1</div>
-        <p className="h-fit">title</p>
-      </div>
+      {coursedata.length > 0 && (
+        coursedata?.map((v,i)=>
+        <div className="border rounded-lg p-4 max-h-48 min-h-48 lg:max-h-72 lg:min-h-80 hover:shadow-md transition-all duration-500 bg-white grid gap-2">
+          <p className="break-normal font-bold tracking-widest border text-gray-600">{v.courseName.trim()}</p>
+          <div className="border bg-gray-50"></div>
+        </div>
+        )
+      )}
     </div>
   );
 };
