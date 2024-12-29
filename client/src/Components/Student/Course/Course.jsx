145dd2fedf0bc56efc922fe9fbfa48fe4a565a22
Ajith kanna T <108063094+ajithkanna1234@users.noreply.github.com>
2024-12-29 20:45:09 +0530

    filter and card layout

--- client/src/Components/Student/Course/Course.jsx
+++ client/src/Components/Student/Course/Course.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useMemo, useState } from "react";
 import Filter from "./Filter";
 import CourseCards from "./CourseCards";
 import { GET } from "../../ApiFunction/ApiFunction";
-import axios from "axios";
 
 const Course = () => {
-
   const [coursedata, setCoursedata] = useState([]);
-  const [filterText, setFilterText] = useState("");
-  
+  const [filterText, setFilterText] = useState({
+    title: "",
+    price: "",
+    rate: "",
+  });
+
   useEffect(() => {
     getData();
   }, []);
@@ -24,14 +26,54 @@ const Course = () => {
   };
 
   const filteredData = useMemo(() => {
-    return coursedata.filter((course) =>
-      course.courseName.toLowerCase().includes(filterText.toLowerCase())
-    );
+    return coursedata.filter((course) => {
+      const matchesTitle = course.courseName
+        .toLowerCase()
+        .includes(filterText.title.toLowerCase());
+
+      let matchesPrice = true;
+      switch (filterText.price) {
+        case 0:
+          matchesPrice = course.price >= 0 && course.price < 800;
+          break;
+        case 1:
+          matchesPrice = course.price >= 800 && course.price <= 1000;
+          break;
+        case 2:
+          matchesPrice = course.price > 1000;
+          break;
+        case 3:
+          matchesPrice = course.price === 0;
+          break;
+        default:
+          matchesPrice = true;
+      }
+
+      let matchesRating = true;
+      switch (filterText.rate) {
+        case 4:
+          matchesRating = parseInt(course.rating) <= 4;
+          break;
+        case 5:
+          matchesRating = parseInt(course.rating) > 4;
+          break;
+        default:
+          matchesRating = true;
+      }
+
+      return matchesTitle && matchesPrice && matchesRating;
+    });
   }, [coursedata, filterText]);
-  
+
   return (
-    <main className="h-[90vh] flex md:flex-row flex-col gap-2 items-start w-full relative bg-gray-50 md:overflow-hidden">
-      <Filter onChange={(e)=>setFilterText(e.target.value)}/>
+    <main className="h-[90vh] md:h-fit flex md:flex-row flex-col gap-2 items-start w-full relative bg-gray-50 ">
+      <Filter
+        setTitle={(e) =>
+          setFilterText((prev) => ({ ...prev, title: e.target.value }))
+        }
+        setPrice={(e) => setFilterText((prev) => ({ ...prev, price: e }))}
+        setRate={(e) => setFilterText((prev) => ({ ...prev, rate: e }))}
+      />
       <CourseCards coursedata={filteredData} />
     </main>
   );
--- client/src/Components/Student/Course/CourseCards.jsx
+++ client/src/Components/Student/Course/CourseCards.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Rate } from "antd";
 
-const CourseCards = ({coursedata}) => {
-  const url = "http://localhost:3000"
-
-  console.log(coursedata);
+const CourseCards = ({ coursedata }) => {
+  const [card, setCard] = useState(null);
+  const url = "http://localhost:3000";
 
   return (
-    <div className="h-fit w-full p-2 lg:pt-4 grid gap-2 lg:gap-4 grid-cols-2 md:grid-cols-3 lg:flex lg:flex-wrap">
+    <div className="h-fit w-full p-2 lg:pt-4 grid gap-2 lg:gap-4 grid-cols-2 md:grid-cols-3 lg:flex lg:flex-wrap transition-all duration-700">
       {coursedata.length > 0 &&
         coursedata?.map((v, i) => (
-          <div className="border rounded-lg p-4 max-h-52 min-h-48 lg:max-h-72 lg:min-h-80 lg:max-w-72 lg:min-w-72 hover:shadow-md transition-all duration-500 bg-white flex flex-col gap-2 relative">
+          <button
+            key={i}
+            className="border rounded-lg p-4 max-h-64 min-h-48 lg:max-h-72 lg:min-h-80 lg:max-w-72 lg:min-w-72 hover:shadow-md transition-all duration-500 bg-white flex flex-col gap-2 relative"
+            onClick={() => setCard(v)}
+          >
             <div className=" bg-gray-50 h-full">
-              <img src={url+v.imagePath} className="rounded-md h-full"/>
+              <img src={url + v.imagePath} className="rounded-md h-full" />
             </div>
             <div className="text-xs md:text-base flex items-center justify-between font-bold tracking-widest text-gray-600">
               <p>{v?.courseName.trim()}</p>
@@ -23,11 +26,13 @@ const CourseCards = ({coursedata}) => {
               disabled
               className="text-xs"
             />
-            <div className="w-full flex items-center justify-between mt-4">
-              <p className="font-bold tracking-wider text-Primary">Price</p>
-              <p className="font-mono">{v?.price ? v?.price : "free"}</p>
+            <div className="w-full text-xs md:text-base flex items-center justify-between mt-4">
+              <p className="font-bold tracking-wider text-gray-500">Price</p>
+              <p className="font-mono text-Primary">
+                {v?.price ? "$" + " " + v?.price : "free"}
+              </p>
             </div>
-          </div>
+          </button>
         ))}
     </div>
   );
