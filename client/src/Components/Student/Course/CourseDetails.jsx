0b759e5c8c196ee266a6262dcea77c3ecf2afd07
Ajith kanna T <ajithkanna78@gmail.com>
2025-01-02 16:19:24 -0800

    course details added

--- client/src/Components/Student/Course/CourseDetails.jsx
+++ client/src/Components/Student/Course/CourseDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GET } from "../../ApiFunction/ApiFunction";
 import { Collapse } from "antd";
-import { CaretRightOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import { CaretRightOutlined, DoubleLeftOutlined, VideoCameraOutlined } from "@ant-design/icons";
 import CustomButton from "../../Common/CustomButton";
 
 const CourseDetails = () => {
@@ -46,17 +46,23 @@ const CourseDetails = () => {
     <>
       {temp.length > 0 &&
         temp.map((item, index) => (
-          <div className="sm:flex bg-gray-100 h-full md:h-[90vh] w-full border relative">
-            <div className="sm:w-fit bg-white border flex flex-col">
-              <h1 className="lg:text-xl font-semibold border-b p-4">
-                {item.courseName}
+          <div className="sm:flex h-full md:h-[90vh] w-full border relative">
+            <div className="sm:w-fit bg-white border flex gap-4 p-4 flex-col">
+              <h1 className="lg:text-xl font-semibold pb-2 ">
+                <DoubleLeftOutlined className="text-Primary bg-Primary/10 hover:scale-125 duration-300 mr-2 rounded-full p-1 border-Primary !text-base"/>
+                Course Details
               </h1>
+              <p className="bg-Primary/10 rounded-md text-Primary p-2 text-center mb-2 tracking-widest">{item.courseName}</p>
+              <p className="text-xs md:text-sm text-Primary">Requirements</p>
+              <div className="text-xs max-w-96 p-2 min-h-40 ">
+                {item.title.map(v=><ol className="list-disc list-inside leading-relaxed"><li>{v}</li></ol>)}
+              </div>
             </div>
-            <div className=" h-full sm:py-2 md:p-2 sm:pr-0 flex-1 overflow-y-scroll">
-              <p className="bg-white p-4 text-sm lg:text-base text-Primary">
+            <div className=" h-full bg-gray-100 sm:pr-0 sm:pb-0 flex-1 overflow-y-scroll">
+              <p className="bg-white p-4 m-2 mb-0 text-sm lg:text-base text-Primary">
                 video <VideoCameraOutlined className="ml-2" />
               </p>
-              <div className="p-4 flex gap-1 bg-white relative ">
+              <div className="p-4 gap-1 mx-2 bg-white relative ">
                 {!item.isPaid && (
                   <div
                     className={`h-full backdrop-grayscale w-full top-0 left-0 absolute`}
@@ -65,7 +71,7 @@ const CourseDetails = () => {
                 <video
                   controls={item.isPaid}
                   muted
-                  className="h-full w-full mx-auto"
+                  className="h-full bg-gray-100 w-full mx-auto"
                 >
                   <source
                     src={`http://localhost:3000${item.videoPath}`}
@@ -73,6 +79,9 @@ const CourseDetails = () => {
                   />
                   Your browser does not support the video tag.
                 </video>
+              <div className="mt-4">
+                <p className="text-sm font-medium">Duration : <span className="rounded-lg font-light text-xs tracking-widest bg-gray-100 p-1 px-2 capitalize">{item.duration}</span></p>
+              </div>
               </div>
               <Collapse
                 bordered={false}
@@ -84,17 +93,18 @@ const CourseDetails = () => {
                   />
                 )}
                 items={subItems(item, index)}
-                className="bg-white lg:px-4 py-4"
+                className="bg-white lg:px-4 py-4 mx-2"
               />
-              <div className="drop-shadow-lg border text-white flex items-center justify-between bg-Primary p-2 backdrop-blur-sm w-full sticky left-0 bottom-0 ">
+              {!item.isPaid &&
+              <div className="drop-shadow-lg flex items-center justify-between p-2 bg-gradient-to-r from-white to-transparent backdrop-blur-sm w-full border-t sticky left-0 bottom-0 ">
                 <p className="font-bold font-Poppins !tracking-wider">Price</p>
                 <p className="mr-auto ml-4 font-medium ">{temp[0].price}</p>
                 <CustomButton
                   title="buy"
                   color="solid"
-                  className="bg-white text-Primary"
+                  className="bg-Primary"
                 />
-              </div>
+              </div>}
             </div>
           </div>
         ))}
