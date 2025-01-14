974852f0ab15abf155421956c5914b1a15838a4f
Sai472002 <saidhasun0407@gmail.com>
2025-01-14 14:44:18 +0530

    .

--- client/src/Routers.js
+++ client/src/Routers.js
@@ -11,13 +11,9 @@ import MainLayout from "./Components/Student/Home/MainLayout";
 import DashBoard from "./Components/Instructor/Dashboard"; 
 import InstructorProfile from "./Components/Instructor/InstructorProfile";
 import InstructorCourse from "./Components/Instructor/InstructorCourse";
-
 import AddCourse from "./Components/Instructor/AddCourse";
 import EditCourse from "./Components/Instructor/EditCourse";
 import Course from "./Components/Student/Course/Course";
-
-
-
 import CourseContent from "./Components/Student/Course/CourseContent";
 import Success from "./Components/Student/Course/SuccessPage";
 import Cancel from "./Components/Student/Course/CancelPage";
