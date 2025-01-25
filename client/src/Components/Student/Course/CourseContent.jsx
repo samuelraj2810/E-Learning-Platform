23cb2b7ea00afc5a6eab0699b151344d977f2abf
Sai472002 <saidhasun0407@gmail.com>
2025-01-25 13:00:14 +0530

    bug fix

--- client/src/Components/Student/Course/CourseContent.jsx
+++ client/src/Components/Student/Course/CourseContent.jsx
@@ -20,7 +20,7 @@ const CourseDetails = () => {
 
   // Load Stripe outside of a component’s render to avoid reloading the Stripe object
   const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-
+  const userId = sessionStorage.getItem("id");
   const getCourse = async () => {
     const data = await GET(`http://localhost:3000/getcourse/${_id}`);
     setTemp(data);
@@ -107,7 +107,7 @@ const CourseDetails = () => {
                 video <VideoCameraOutlined className="ml-2" />
               </p>
               <div className="p-4 gap-1 mx-2 bg-white relative ">
-                {item.boughtBy.length == 0 && (
+                {!item.boughtBy.includes(userId)  && (
                   <div
                     className={`h-full backdrop-grayscale w-full top-0 left-0 absolute`}
                   />
@@ -144,21 +144,19 @@ const CourseDetails = () => {
                 items={subItems(item, index)}
                 className="bg-white lg:px-4 py-4 mx-2"
               />
-              {item.boughtBy.length == 0 && (
-                <div className="drop-shadow-lg flex items-center justify-between p-2 bg-gradient-to-r from-white to-transparent backdrop-blur-sm w-full border-t sticky left-0 bottom-0 ">
-                  <p className="font-bold font-Poppins !tracking-wider">
-                    Price
-                  </p>
-                  <p className="mr-auto ml-4 font-medium ">
-                    {item.price || "free"}
-                  </p>
-                  <CustomButton
-                    title="buy"
-                    color="solid"
-                    className="bg-Primary"
-                    onClick={() => handleBuyClick(item)}
-                  />
-                </div>
+              {!item.boughtBy.includes(userId) && (
+              <div className="drop-shadow-lg flex items-center justify-between p-2 bg-gradient-to-r from-white to-transparent backdrop-blur-sm w-full border-t sticky left-0 bottom-0 ">
+                <p className="font-bold font-Poppins !tracking-wider">Price</p>
+                <p className="mr-auto ml-4 font-medium ">
+                  {item.price || "free"}
+                </p>
+                <CustomButton
+                  title="buy"
+                  color="solid"
+                  className="bg-Primary"
+                  onClick={() => handleBuyClick(item)}
+                />
+              </div>
               )}
             </div>
           </div>
