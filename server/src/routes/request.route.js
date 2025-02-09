1488c8b4f939a2b38953740f19bf7ebb5f61b34d
Sai472002 <saidhasun0407@gmail.com>
2025-02-09 17:22:18 +0530

    date added

--- server/src/routes/request.route.js
+++ server/src/routes/request.route.js
@@ -1,31 +1,34 @@
-const express = require("express")
-const Request = require("../models/Request.model") 
-const router = express.Router()
+const express = require("express");
+const Request = require("../models/Request.model");
+const router = express.Router();
+const { format } = require("date-fns");
+router.post("/request", async (req, res) => {
+  const now = new Date();
+  const Date = format(now, "MMM dd yyyy hh:mm a");
+  console.log(formattedDateTime);
 
-router.post("/request",async(req,res)=>{
-    try {
-        const {courseid , coursename} = req.body
-        const data = {
-            courseid,
-            coursename
-        }
-        await Request.create(data)
-        res.json({
-            message:"Request Submitted"
-        })
-    } catch (error) {
-        res.json(error.message)   
-    }
-})
+  try {
+    const { courseid, coursename } = req.body;
+    const data = {
+      courseid,
+      coursename,
+      createdAt:Date
+    };
+    await Request.create(data);
+    res.json({
+      message: "Request Submitted",
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
-router.get("/getRequests",async(req,res)=>{
-    try {
-        const data = await Request.find()
-        res.json(data)
-    } catch (error) {
-        res.json(error.message)
-        
-    }
-
-})
-module.exports = router
+router.get("/getRequests", async (req, res) => {
+  try {
+    const data = await Request.find();
+    res.json(data);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+module.exports = router;
