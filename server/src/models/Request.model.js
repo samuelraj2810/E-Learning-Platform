16203b5b54c99a90f2e197b89e7f722ca053f6b2
Sai472002 <saidhasun0407@gmail.com>
2025-02-09 15:24:04 +0530

    push

--- server/src/models/Request.model.js
+++ server/src/models/Request.model.js
@@ -1,19 +1,19 @@
-const mongoose =require("mongoose")
-const {v4} = require("uuid")
+const mongoose = require("mongoose");
+const { v4 } = require("uuid");
 const ReqSchema = new mongoose.Schema({
-    _id:{
-        type:String,
-        default:v4
-    },
-    courseid:String,
-    coursename:String,
-    status:{
-        type:String,
-        default:"Pending"
-    },
-    description:String
-})
+  _id: {
+    type: String,
+    default: v4,
+  },
+  courseid: String,
+  coursename: String,
+  status: {
+    type: String,
+    default: "Pending",
+  },
+  description: { type: String, default: "Request To Delete This Course" },
+});
 
-const Request = mongoose.Model("Requests",ReqSchema)
+const Request = mongoose.model("Requests", ReqSchema);
 
-module.exports = Request
+module.exports = Request;
